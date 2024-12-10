12de03b95aefc1be23a92fa10c8c2fa86b24144e
OliverJ-LCWC <144791858+OliverJ-LCWC@users.noreply.github.com>
2024-12-10 13:35:09 +0000

    woo

--- bank_account.py
+++ bank_account.py
@@ -1,1 +1,18 @@
+class Bank_account:    
+    def __init__(self, balance: int, owner: str):
+        self.__balance = balance
+        self.__owner = owner
 
+    def get_balance(self):
+        return self.__balance
+    def get_owner(self):
+        return self.__owner
+    def set_owner(self, owner: str):
+        self.__owner = owner
+    def deposit(self, amount: float):
+        if amount > 0:
+            self.__balance += amount
+    def withdraw(self, amount: float):
+        if 0 < amount < self.__balance:
+            self.__balance -= amount
+        else: exit
